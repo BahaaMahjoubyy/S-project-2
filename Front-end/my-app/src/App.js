fb2556a28e14742712a30a4a01c0709b4c804921
amenybettaher <bettaherameni1919@gmail.com>
2024-02-05 22:01:27 +0100

    navbar and home page image done

--- Front-end/my-app/src/App.js
+++ Front-end/my-app/src/App.js
@@ -1,45 +1,47 @@
-import React ,{useState}from 'react'
-import News  from './components/News.jsx';
-import '../src/App.css'
-function App() {
+// Import necessary dependencies
+import React, { useState } from 'react';
+import About from './components/About.jsx'; // Adjust the path as needed
+import News from './components/News.jsx';
+import '../src/App.css';
+import home from '../src/images/home.png';
 
+// Define the App component
+function App() {
+  // Initialize the view state with 'Home'
   const [view, setView] = useState('Home');
 
+  // Function to change the view
   const changeView = (newView) => {
     setView(newView);
   };
 
-
-
-
-
+  // Return the JSX structure
   return (
     <div className="App">
-     <nav className='navbar'>
-        {/* <img className='logo' src={logo} alt="Logo" /> */}
+      {/* Navbar with menu items */}
+      <nav className='navbar'>
         <h2 onClick={() => changeView('Home')}>Home</h2>
         <h2 onClick={() => changeView('CodesNews')}>Codes News</h2>
         <h2 onClick={() => changeView('Postes')}>Postes</h2>
         <h2 onClick={() => changeView('Chat')}>Chat</h2>
-
-
-
       </nav>
-      <div>
 
+      {/* Horizontal line under the navbar */}
+      <hr></hr>
 
+      {/* Render the image on the 'Home' view */}
+      {view === 'Home' && <img className='home' src={home} alt="Home" />}
 
-     
-      {view === 'CodesNews' && < News view={view}/> }
+      {/* Conditionally render the Homy component based on the view state */}
+      {view === 'Home' && <About />}
 
-        
-        
-  
-      </div>
-   
+      {/* Conditionally render the News component based on the view state */}
+      {view === 'CodesNews' && <News view={view} />}
     </div>
-   
   );
-};
+}
 
+// Export the App component
 export default App;
+
+
