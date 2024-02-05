e10c6ee83052e54057286f4345b1ea30737a09d6
CheniIyes <cheniiyes@gmail.com>
2024-02-05 15:15:17 +0100

    user-back-end

---
+++ Back-end/Controllers/users.js
@@ -0,0 +1,22 @@
+const model = require("../Models/users");
+module.exports = {
+      add: (req, res) => {
+            let newUser = req.body;
+            model.addUser(newUser, (err, results) => {
+                  if (err) {
+                        res.status(500).send(err);
+                  } else {
+                        res.json(results)
+                  }
+            });
+      },
+      DELETE: (req, res) => {
+            model.DELETE(req.params.id, (err, results) => {
+                  if (err) {
+                        res.status(500).send(err);
+                  } else {
+                        res.json(results);
+                  }
+            });
+      },
+};
---
+++ Back-end/Models/users.js
@@ -0,0 +1,23 @@
+const connection = require("../database/index")
+
+
+module.exports = {
+      addUser: (newUser, callback) => {
+            const sql = "INSERT INTO user SET username=?, email=?, password=?"
+            connection.query(sql, [newUser.username, newUser.email, newUser.password], (err, result) => {
+                  callback(err, result)
+            })
+      },
+      login: (data, callback) => {
+            const sql = "SELECT * FROM user WHERE email=? AND password=?"
+            connection.query(sql, [data.email, data.password], (err, result) => {
+                  callback(err, result)
+            })
+      },
+      DELETE: (id, callback) => {
+            const sql = "DELETE FROM user WHERE id=?"
+            connection.query(sql, [id], (err, results) => {
+                  callback(err, results)
+            })
+      },
+}
