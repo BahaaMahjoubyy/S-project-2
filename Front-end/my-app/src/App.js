84906671e894b35256c76449896eb3a33e906ceb
amenybettaher <bettaherameni1919@gmail.com>
2024-02-06 12:52:39 +0100

    about section

--- Front-end/my-app/src/App.js
+++ Front-end/my-app/src/App.js
@@ -1,16 +1,14 @@
-
 import React, { useState } from 'react';
 import About from './components/About.jsx'; 
 import News from './components/News.jsx';
 import '../src/App.css';
 import home from '../src/images/home.png';
 
-import Login from './components/Login.jsx';
-import SignIn from './components/SignIn.jsx';
+// import Login from './components/Login.jsx';
+// import SignIn from './components/SignIn.jsx';
+
 function App() {
-
   const [view, setView] = useState('Home');
-
 
   const changeView = (newView) => {
     setView(newView);
@@ -18,23 +16,23 @@ function App() {
 
   return (
     <div className="App">
-   
       <nav className='navbar'>
         <h2 onClick={() => changeView('Home')}>Home</h2>
         <h2 onClick={() => changeView('CodesNews')}>Codes News</h2>
         <h2 onClick={() => changeView('Postes')}>Postes</h2>
         <h2 onClick={() => changeView('Chat')}>Chat</h2>
+        <h2 onClick={() => changeView('About')}>About</h2> {/* Update view to 'About' */}
         <h2 className="Login" onClick={() => changeView('Login')}>🔻 Login</h2>
-
-
-
       </nav>
 
       <hr></hr>
 
-      {view === 'Home' && <img className='home' src={home} alt="Home" />}
-
-      {view === 'Home' && <About />}
+      {view === 'Home' && (
+        <>
+          <img src={home} alt="Home" />
+          <About />
+        </>
+      )}
 
       {view === 'CodesNews' && <News view={view} />}
     </div>
@@ -42,5 +40,6 @@ function App() {
 }
 
 export default App;
+
 
 
